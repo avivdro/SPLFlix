a25ae365f2e3ad52c273c5485f7bae1c769d7da9
Aviv <avivdro@post.bgu.ac.il>
2021-05-24 01:46:26 +0300

    work on User

--- User.cpp
+++ User.cpp
@@ -11,6 +11,49 @@ using namespace std;
 User::User(const std::string &name)
     : history(), name(name) {}
 
+//copy ctor
+User::User(const User& other) : history(), name(other.name){
+    copy(other);
+}
+
+//copy assignment
+User &User::operator=(const User &other){
+    if (this != &other){
+        clear();
+        copy(other);
+    }
+    return *this;
+}
+//move ctor
+User::User(User &&other) : history(), name(other.name){
+    move(std::move(other));
+}
+//move assignment
+User &User::operator=(User &&other){
+    if (this != &other){
+        clear();
+        move(std::move(other));
+    }
+    return *this;
+}
+//destructor
+User::~User(){
+    clear();
+}
+//clone
+//add to history
+void User::addToHistory(Watchable *w) {
+    history.push_back(w);
+}
+//is in history?
+bool User::isInHistory(const Watchable *w) const{
+    for (const auto &content: history){
+        if (*content == *w)
+            return true;
+    }
+    return false;
+}
+
 std::string User::getName() const {
     return name;
 }
@@ -18,6 +61,30 @@ std::string User::getName() const {
 std::vector<Watchable *> User::get_history() const {
     return history;
 }
+
+void User::copy(const User &other){
+    for (auto wPtr : other.history){
+        // history.push_back(wPtr->clone()); //TODO implement clone
+    }
+}
+
+void User::clear(){
+    for (auto &wPtr : history){
+        delete wPtr;
+        wPtr = nullptr;
+    }
+    history.clear();
+}
+
+void User::move(User &&other){
+    for (auto &wPtr : other.history){
+        history.push_back(wPtr);
+        wPtr = nullptr;
+    }
+}
+
+
+//CHILD CLASSES----------------------
 
 LengthRecommenderUser::LengthRecommenderUser(const std::string &name) : User(name) {
 
--- User.h
+++ User.h
@@ -15,17 +15,36 @@ class Session;
 class User{
 public:
     //BUILT IN - UNCHANGEABLE
+    //ctor
     User(const std::string& name);
     virtual Watchable* getRecommendation(Session& s) = 0;
     std::string getName() const;
     std::vector<Watchable*> get_history() const;
+    //OURS-
+    //copy ctor
+    User(const User& other);
+    //copy assignment
+    User &operator=(const User &other);
+    //move ctor
+    User(User &&other);
+    //move assignment
+    User &operator=(User &&other);
+    //destructor
+    virtual ~User();
+    virtual User *clone(std::string &name) = 0;
+    void addToHistory(Watchable *w);
+    bool isInHistory(const Watchable *w) const;
+
 protected:
     //BUILT IN - UNCHANGEABLE
     std::vector<Watchable*> history;
 private:
     //BUILT IN - UNCHANGEABLE
     std::string name;
-
+    //OURS:
+    void copy(const User &other);
+    void clear();
+    void move(User &&other);
 };
 
 
